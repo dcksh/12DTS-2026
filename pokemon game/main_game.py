52eab2a1645bc68d4a2b88cfe7a9b9ccc15fc831
dcksh <23542@wc.school.nz>
2026-03-03 11:38:54 +1300

    pokemon game

---
+++ pokemon game/main_game.py
@@ -0,0 +1,2 @@
+import pokemon_library as library
+
---
+++ pokemon game/pokemon_library.py
@@ -0,0 +1,26 @@
+# pokemon library!
+# stores all the data for wild and own pokemon!
+# daksh shah
+
+# last updated - 2/3/26
+
+# !!FOR WILD POKEMON!! please add...
+# name
+# type
+# level
+# health
+# attacks
+# --EXTENSIONS--
+# items
+# whatever else you want
+
+WILD_POKEMON = [
+    {"Name" : "Pikipek",
+    "Type" : "Normal"},
+
+    {"Name": "Yungoos",
+     "Type": "Normal"},
+
+    {"Name": "Grubbin",
+     "Type": "Bug"}
+]
