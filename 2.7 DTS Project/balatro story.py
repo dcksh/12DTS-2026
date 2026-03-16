5a352abec60f5f8e93712461466b281e0f38d42a
dcksh <23542@wc.school.nz>
2026-03-16 15:11:40 +1300

    I added in my new DTS project as a folder, which contains code for balatro story.py

--- 2.7 DTS Project/balatro story.py
+++ 2.7 DTS Project/balatro story.py
@@ -1,10 +1,14 @@
 # balatro
 # roguelike game where you score hands to beat blinds, and grab jokers to upgrade your hands against stronger blinds.
+# 2.7 dts assessment
+# daksh shah
+# 10/3/26
 
-# daksh
-# 20/2/26
+# last updated - 10/3/26
 
-# last updated - 28/2/26
+# // STORY //
+# Follows the story of Astus, a Jester from Ancient Rome.
+
 # !! SO FAR !!
 # - Deals a hand to the user
 # - User may either sort the cards by rank/suit or select cards to play/discard
@@ -31,8 +35,8 @@ card_selection_loop = True
 
 # constants
 SUIT = ["Hearts", "Diamonds", "Clubs", "Spades"]
-CARDS = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
-FACE_INITIALS = ["J, Q, K"]
+CARDS = ["Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"]
+FACE_INITIALS = ["J, Q, K, A"]
 POKER_HANDS = {
     "High Card" : [5,1],
     "Pair" : [10,2],
@@ -50,8 +54,8 @@ POKER_HANDS = {
 } # multipliers for each hand - stored by chip addition, then chip multiplier
 BLIND_BASE_CHIPS = [300, 800, 2000, 5000, 11000, 20000, 35000, 50000] # scores for each anti, base chips increase per anti
 BLIND_MULTIPLIERS = [[1,"Small"],[1.5,"Big"],[2,"Boss"]] # multiplier based on boss
-HAND_SIZE = 8
-SELECT_HAND_SIZE = 5
+HAND_SIZE = 32
+SELECT_HAND_SIZE = 6
 
 # functions
 def blind_maker(): # makes the current blind
@@ -71,12 +75,16 @@ def blind_maker(): # makes the current blind
 
 def new_deck(): # makes a new deck
     card_deck.clear() # clear old usae
-    for i in range(1, 11): # prints out the number cards, uses for loops to make 44 of these
+    for i in range(1, 10): # prints out the number cards, uses for loops to make 44 of these
         for j in range(4):
-            card_deck.append([SUIT[j], CARDS[i - 1], i, i])
-    for k in range(10, 13): # same as above but face cards
+            card_deck.append([SUIT[j], CARDS[i - 1], i+1, i+1])
+    for k in range(9, 13): # same as above but face cards + ace
         for j in range(4):
-            card_deck.append([SUIT[j], CARDS[k], 10, k+1])
+            if k == 12:
+                card_deck.append([SUIT[j], CARDS[k], 11, k + 2]) # different for ace as ace has 11 points.
+            else:
+                card_deck.append([SUIT[j], CARDS[k], 10, k+2])
+    print(card_deck) # DEBUG
 
 
 def hand_deal(amount): # deals you the hand
@@ -117,7 +125,8 @@ def gameplay(): # main gameplay loop
                     hand.sort(key=lambda x: (x[3], [0]))
                     order_state = 1
                 for j in range(0, len(hand)): # prints out the new sorting order, loop doesnt end as they have to play first
-                    print(j + 1, hand[j][1], "of", hand[j][0])
+                    print(hand[j][4], hand[j][1], "of", hand[j][0])
+
                 time.sleep(1)
             elif choice == "2": # chosen to select
                 choice_loop = False
@@ -130,6 +139,7 @@ def gameplay(): # main gameplay loop
                             # strip then gets rid of extra stuff (white spaces by default) to leave it as the number alone, allowing the code to convert it into an integer
                             # then, just adds it to the list fo selected cards
                             selection_list.append(int(i.strip()))
+
                     except ValueError: # error fixing
                         print()
                         print("Please enter a max of",SELECT_HAND_SIZE,"cards, and refer to the number at the start!")
@@ -153,6 +163,7 @@ def gameplay(): # main gameplay loop
                                         finding_card_loop = False
                         card_selection_loop = False
                 score_calculation(chosen_cards)
+
             else: # error checking
                 print("Please select either '1' or '2'!")
 
@@ -162,15 +173,15 @@ def score_calculation(cards):
     chip_mult = 0
     ranks = {}
     suits = {}
-    straight_check = False
     straight = []
-    straight_count = 1
+    straight_count = 0
+    straight_check = False
+    royal_check = False
+    full_house_check = False
+    flush_check = False
 
     cards.sort(key=lambda x: (x[3]))
     print(cards) # DEBUG
-
-    for i in range(len(cards)): # adds base chip values of the cards first
-        chip_score += cards[i][2]
 
     for i in range(len(cards)): # sorts the deck's ranks out
         if cards[i][3] in ranks:
@@ -178,7 +189,7 @@ def score_calculation(cards):
         else:
             ranks[cards[i][3]] = 1
     ranks = dict(sorted(ranks.items(), key=lambda i: i[1]))
-    print(ranks)
+    print(ranks) # DEBUG
 
     for i in range(len(cards)): # sort the hand's suits out
         if cards[i][0] in suits:
@@ -186,29 +197,68 @@ def score_calculation(cards):
         else:
             suits[cards[i][0]] = 1
     suits = dict(sorted(suits.items(), key=lambda i: i[1]))
-    print(suits)
+    print(suits) # DEBUG
 
     # checks for straights - sorts the keys (using sort removes duplicates), and then checks to see if its consecutive
     # if it fails, turn to 1 and continue (take into account hands that are above 5), and if it hits 5, say straight is true
+    # NEED TO UPDATE FOR HIGH AND LOW ACES!! SO FAR CAN DO HIGH!!!
     straight = sorted(ranks.keys())
-    print(straight)
+    print(straight) # DEBUG
+    if 14 in straight: # adds in a value of 1 if there is a straight so both high and low ace exists in the list.
+        straight.append(1)
+        straight.sort()
+    print(straight)  # DEBUG
     for i in range(1, len(straight)):
         if straight[i] == straight[i - 1] + 1:
             straight_count += 1
-            print(straight_count)
-            if straight_count >= 5:
+            print(straight_count) # DEBUG
+            if straight_count >= 4:
                 straight_check = True
                 straight_count = 0
                 break  # stops loop once straight is found
         else:
             straight_count = 0
 
-    if max(suits.values()) == 5 and max(ranks.values()) == 5:  # FLUSH FIVE - Five cards of the same rank and suit
+    if max(suits.values()) >= 5:
+        flush_check = True
+
+    # checks for royalty - just use for royal flush
+    if 14 in straight and 13 in straight and 12 in straight and 11 in straight and 10 in straight:
+        royal_check = True
+
+    # checks for full house - used separately due to the weird clatter of arguements which will be neater to put under a check here.
+    if 3 in ranks.values() and (2 in ranks.values() or list(ranks.values()).count(3) >= 2):
+        full_house_check = True
+
+    if max(suits.values()) == 5 and max(ranks.values()) == 5:  # FLUSH FIVE - Five cards of the same rank and suit // ADD CHIP SCORING
         chip_score += POKER_HANDS["Flush Five"][0]
         chip_mult += POKER_HANDS["Flush Five"][1]
-    # FLUSH HOUSE - Full House + Flush
-    # FIVE OF A KIND
-    # ROYAL FLUSH - Straight by A K Q J 10
+
+        print("Flush Five")
+
+    elif full_house_check == True and flush_check == True: # FLUSH HOUSE - Full House + Flush // ADD CHIP SCORING
+        chip_score += POKER_HANDS["Flush House"][0]
+        chip_mult += POKER_HANDS["Flush House"][1]
+
+        print("Flush House")
+
+    elif max(ranks.values()) == 5: # FIVE OF A KIND // ADD CHIP SCORING
+        chip_score += POKER_HANDS["Five of a Kind"][0]
+        chip_mult += POKER_HANDS["Five of a Kind"][1]
+
+        print("Five of a Kind")
+
+    elif max(suits.values()) == 5 and straight_check == True and royal_check == True: # ROYAL FLUSH - Flush by A K Q J 10 // straight check is unneeded but just in case...
+        chip_score += POKER_HANDS["Royal Flush"][0]
+        chip_mult += POKER_HANDS["Royal Flush"][1]
+
+        for i in range(5):
+            chip_score += cards[i][2]
+
+        print("Royal Flush!")
+        for i in range(5):
+            print("Scored", cards[i][1], "of", cards[i][0])
+
     elif straight_check == True and max(suits.values()) == 5:  # STRAIGHT FLUSH - Straight + Flush
         chip_score += POKER_HANDS["Straight Flush"][0]
         chip_mult += POKER_HANDS["Straight Flush"][1]
@@ -218,20 +268,23 @@ def score_calculation(cards):
 
         print("Straight Flush!")
         for i in range(5):
-            print("Scored",cards[i][1],"of",cards[i][0],".")
+            print("Scored",cards[i][1],"of",cards[i][0])
 
-    elif max(ranks.values()) == 4:  # FOUR OF A KIND
-        chip_score += POKER_HANDS["Four"][0]
-        chip_mult += POKER_HANDS["Four"][1]
+    elif max(ranks.values()) == 4:  # FOUR OF A KIND // ADD CHIP SCORING
+        chip_score += POKER_HANDS["Four of a Kind"][0]
+        chip_mult += POKER_HANDS["Four of a Kind"][1]
 
         print("Four of a Kind!")
 
-    # FULL HOUSE - Three of a kind + Two of a kind
+    elif full_house_check == True: # FULL HOUSE - Three of a kind + Two of a kind // ADD CHIP SCORING
+        chip_score += POKER_HANDS["Full House"][0]
+        chip_mult += POKER_HANDS["Full House"][1]
+
+        print("Full House")
 
     elif max(suits.values()) == 5:  # FLUSH - All cards have 1 suit
         chip_score += POKER_HANDS["Flush"][0]
         chip_mult += POKER_HANDS["Flush"][1]
-
 
         for i in range(5):
             chip_score += cards[i][2]
@@ -240,7 +293,7 @@ def score_calculation(cards):
         for i in range(5):
             print("Scored",cards[i][0],"of",cards[i][1])
 
-    elif straight_check == True:  # STRAIGHT - All cards are consecutive. Order - A K Q J 10... 1
+    elif straight_check == True:  # STRAIGHT - All cards are consecutive. Order - A K Q J 10... 3 2 A
         chip_score += POKER_HANDS["Straight"][0]
         chip_mult += POKER_HANDS["Straight"][1]
 
@@ -251,27 +304,31 @@ def score_calculation(cards):
         for i in range(5):
             print("Scored",cards[i][0],"of",cards[i][1])
 
-    elif max(ranks.values()) == 3:  # THREE OF A KIND
+    elif max(ranks.values()) == 3:  # THREE OF A KIND // ADD CHIP SCORING
         chip_score += POKER_HANDS["Three of a Kind"][0]
         chip_mult += POKER_HANDS["Three of a Kind"][1]
 
         print("Three of a Kind!")
 
-    # TWO PAIR
+    elif list(ranks.values()).count(2) >= 2: # TWO PAIR // ADD CHIP SCORING
+        chip_score += POKER_HANDS["Two Pair"][0]
+        chip_mult += POKER_HANDS["Two Pair"][1]
 
-    elif max(ranks.values()) == 2:  # PAIR
+        print("Two Pair!")
+
+    elif max(ranks.values()) == 2:  # PAIR // ADD CHIP SCORING
         chip_score += POKER_HANDS["Pair"][0]
         chip_mult += POKER_HANDS["Pair"][1]
 
         print("Pair!")
 
-    elif max(ranks.values()) == 1:  # HIGH CARD
+    elif max(ranks.values()) == 1:  # HIGH CARD // ADD CHIP SCORING
         chip_score += POKER_HANDS["High Card"][0]
         chip_mult += POKER_HANDS["High Card"][1]
 
         print("High Card!")
 
-    return chip_score, chip_mult
+    return chip_score, chip_mult # // AT ENDDD! //
 
 # ------------------------------- main module -------------------------------
 print("Welcome to Balatro!")
